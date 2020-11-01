407829c8034e65828ba633ab1cb48d27ff238fb1
Brent Soles <brent@pop-os.localdomain>
2020-11-01 12:07:03 -0800

    scaffold/main: initial commit

---
+++ third_party/glew-2.1.0/build/CMakeFiles/visualinfo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visualinfo.dir/home/brent/apps/cpp/learnopengl/third_party/glew-2.1.0/src/visualinfo.c.o"
+  "bin/visualinfo"
+  "bin/visualinfo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/visualinfo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
